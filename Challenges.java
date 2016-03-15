f385ff8571167a6cc3b438d28c5dfd052a68e6fc
Leonardo Peta <leonardopeta@live.co.uk>
2016-03-15 19:31:37 +0000

    update new funtionality

--- Challenges.java
+++ Challenges.java
@@ -7,4 +7,17 @@ public class Challenges {
   public int sum(int x, int y) {
     return x + y;
   }
+
+  public String firsFive(String name) {
+    if(name.length() < 5) {
+      return name;
+    }
+
+    return name.substring(0, 5);
+  }
+
+  public int timesFive(int number) {
+    return number * 5;
+  }
+
 }
--- Runner.java
+++ Runner.java
@@ -5,5 +5,7 @@ public class Runner {
     Challenges challenges = new Challenges();
 
     System.out.println("sum(3 + 3): " + challenges.sum(3, 3));
+    System.out.println("5 first caracteres: " + challenges.firsFive("leo"));
+    System.out.println("The number * 5 is: " + challenges.timesFive(5));
   }
 }
