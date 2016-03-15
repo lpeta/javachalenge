55a97095abbfb2065968e6964a3c6153a7c15a77
Leonardo Peta <leonardopeta@live.co.uk>
2016-03-15 18:09:16 +0000

    exercicies solution

---
+++ Runner.java
@@ -0,0 +1,17 @@
+public class Runner {
+  int x;
+  int y;
+
+  public static void main(String[] args) {
+
+    System.out.println(Sum(3,3));
+
+  }
+
+  public static int Sum (int x, int y) {
+
+    int output = x + y;
+    return output;
+
+  }
+}
